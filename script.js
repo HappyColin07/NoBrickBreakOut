17d2afa3eb851ca3adc2aeca435ac5f575096024
김해피 <happycolin777@icloud.com>
2022-08-06 14:01:50 +0900

    Add files via upload

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ let ctx = $canvas.getContext("2d");
 let x = $canvas.width/2
 let y = $canvas.height - 30;
 
-
+let score = 0;
 
 let dx = 2;
 let dy = -2;
@@ -19,6 +19,20 @@ let rightPressed = false;
 let leftPressed = false;
 document.addEventListener('keydown',keyDown);
 document.addEventListener('keyup',keyUp);
+
+// document.addEventListener("touchStart", touchstartHandler);
+// document.addEventListener("touchHend", touchendHandler);
+
+// function touchstartHandler(e) {
+//     let touchX = e.changedTouches[0].clientX;
+//     let touchY = e.changedTouches[0].clientY;
+//     if(touchX >= 0 && touchX <= $canvas.width/2) leftPressed = ture;
+//     else if(touchX > $canvas.width/2 && touchX <= $canvas.width) rightPressed = ture;
+// }
+// function touchstartHandler(e) {
+//     rightPressed = false;
+//     leftPressed = false;
+// }
 
 function keyDown(e){
     console.log(e.keyCode);
@@ -56,6 +70,8 @@ function draw(){
     ctx.clearRect(0, 0,$canvas.width, $canvas.height);
     drawBall();
     drawPaddle();
+    ctx.fillText("점수 : ",20,30);
+    ctx.fillText(score,50,30);
     if (x> 390 || x< 10) {
         dx = -dx;
     }
@@ -65,6 +81,7 @@ function draw(){
     else if(y + dy> $canvas.height-10){
         if(x > paddleX && x < paddleX + paddleWidth){
             dy= -dy;
+            score++;
         }
         else{
             alert("게임 오버");
@@ -83,3 +100,9 @@ function draw(){
     }
 
 let set_id = setInterval(draw, 10);
+
+if(isStart) {
+    let set_id = setInterval(draw, 10);
+}else{
+    alert("안녕히 가세요")
+}
